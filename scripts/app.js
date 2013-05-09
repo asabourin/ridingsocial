cf6f7a4d94c49d3977a4d0f15c6f7bc3d422f689
asabourin <antoine.sabourin@gmail.com>
2013-05-09 19:58:26 +1000

    Prod server

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,7 @@
 
 var Settings = {
 
-  //host: 'http://www.ridingsocial.net/api/',
-  host: 'http://192.168.1.2:3000/api/',
+  host: 'http://www.ridingsocial.net/api/',
   coeff: 0.01,
   radius: 15,
   checkin_distance: 12
