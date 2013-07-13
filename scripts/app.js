d40b25504548e3a9a5d25f821ba87d4d28ef4350
asabourin <antoine.sabourin@gmail.com>
2013-07-13 17:41:08 +1000

    Added angular-hammer for touch eevnets

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
 
 angular.module('Services', []);
 
-angular.module('App', ['Services', 'ui.bootstrap', 'google-maps', 'ajoslin.mobile-navigate'])
+angular.module('App', ['Services', 'ui.bootstrap', 'google-maps', 'ajoslin.mobile-navigate', 'hmTouchevents'])
 
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
