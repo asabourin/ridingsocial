c4e17dc0369674363976944e6c62965fb240346e
Antoine Sabourin <antoine@redant.com.au>
2013-07-15 15:01:33 +1000

    Simpler loader on spot and rider

--- scripts/controllers/riders.js
+++ scripts/controllers/riders.js
@@ -2,11 +2,8 @@ angular.module('App')
   
   .controller('Riders.show', function(User, Riders, $scope, $routeParams) {
 
-    $scope.loading = true
-
     Riders.show(User.token(), $routeParams.id, function(response) {
         $scope.rider = response;
-        $scope.loading = false
         $scope.tab = 'sessions'
     })
 
@@ -25,11 +22,9 @@ angular.module('App')
     }
 
     $scope.follow = function() {
-        $scope.loading = true
         Riders.follow(User.token(), $scope.rider.id, function(response) {
             $scope.rider.following = true
             $scope.rider.nb_followers += 1
-            $scope.loading = false
         })
     }
 
@@ -39,11 +34,9 @@ angular.module('App')
 
     function stopFollowing(index) {
         if(index == 1) {
-            $scope.loading = true
             Riders.unfollow(User.token(), $scope.rider.id, function(response) {
                 $scope.rider.following = false
                 $scope.rider.nb_followers -= 1
-                $scope.loading = false
             })
             $scope.$apply()
         }
--- scripts/controllers/spots.js
+++ scripts/controllers/spots.js
@@ -2,13 +2,11 @@ angular.module('App')
   
   .controller('Spots.show', function(User, Spots, Riders, $scope, $routeParams, $window) {
 
-    $scope.loading = true
     $scope.tab = 'sessions'
 
     Spots.show(User.token(), $routeParams.id, function(response) {
         $scope.spot = response;
         $scope.spot.picture = "http://maps.googleapis.com/maps/api/staticmap?center="+$scope.spot.lat+","+$scope.spot.lng+"&zoom=16&size="+$window.innerWidth+"x220&maptype=satellite&sensor=true&markers=icon:http://ridingsocial.net/images/flags/"+$scope.spot.color+"_48.png%7C"+$scope.spot.lat+","+$scope.spot.lng+""
-        $scope.loading = false
     })
 
     Spots.sessions(User.token(), $routeParams.id, function(response) {
@@ -27,10 +25,8 @@ angular.module('App')
 
     function watch(index) {
         if(index == 1) {
-            $scope.loading = true
             Riders.watch(User.token(), $scope.spot.id, function(response) {
                 $scope.spot.watched = true
-                $scope.loading = false
             })
             $scope.$apply()
         }
@@ -42,10 +38,8 @@ angular.module('App')
 
     function unwatch(index) {
         if(index == 1) {
-            $scope.loading = true
             Riders.unwatch(User.token(), $scope.spot.id, function(response) {
                 $scope.spot.watched = false
-                $scope.loading = false
             })
             $scope.$apply()
         }
