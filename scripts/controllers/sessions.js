a7228ab2793ba94dac23cf6b1073554c8c2289b4
asabourin <antoine.sabourin@gmail.com>
2013-07-18 00:19:56 +1000

    Angular controller awesomeness for likes and comments on session partial

--- scripts/controllers/sessions.js
+++ scripts/controllers/sessions.js
@@ -5,13 +5,40 @@ angular.module('App')
 
     $rootScope.showNav = true
 
-    
-
     // Events
 
     $rootScope.$on("sessionsUpdated", function (event, args) {
         $scope.loading = false
         $scope.sessions = args.sessions
-    });
+    })
+
+})
+  .controller('SessionController', function(Sessions, User, $scope, $rootScope) {
+
+    $scope.showComments = function () {
+      Sessions.comments($scope.session.id, function(response) {
+        $rootScope.comments = response
+      })
+      $rootScope.commentsOpen = true;
+    };
+    $scope.hideComments = function () {
+      $rootScope.comments = []
+      $rootScope.commentsOpen = false;
+    };
+
+    $scope.toggleLikeSession = function() {
+      if($scope.session.liked) {
+        Sessions.unlike(User.token(), $scope.session.id, function(response) {
+          $scope.session.liked = false
+          $scope.session.nb_likes -= 1
+        })
+      }
+      else {
+        Sessions.like(User.token(), $scope.session.id, function(response) {
+          $scope.session.liked = true
+          $scope.session.nb_likes += 1
+        })
+      }
+    }
 
 })
