ba20557b3cabd784a8f6a27391d566ff28dc8c9c
Antoine Sabourin <antoine@redant.com.au>
2013-07-22 13:02:34 +1000

    Clean up unused code

--- scripts/controllers/sessions.js
+++ scripts/controllers/sessions.js
@@ -1,18 +1,4 @@
 angular.module('App')
-  .controller('Sessions.followed', function(Sessions, User, $scope, $rootScope, $location) {
-
-    // Init
-
-    $rootScope.showNav = true
-
-    // Events
-
-    $rootScope.$on("sessionsUpdated", function (event, args) {
-        $scope.loading = false
-        $scope.sessions = args.sessions
-    })
-
-})
   .controller('SessionController', function(Sessions, User, $scope, $rootScope) {
 
     $scope.showOthers = function (session_id) {
