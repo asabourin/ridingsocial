85072df206f333485804645ca97f108b1e1c89ec
Antoine Sabourin <antoine@redant.com.au>
2013-07-30 16:59:54 +1000

    Cleanup after running jshint

--- scripts/services/checkin.js
+++ scripts/services/checkin.js
@@ -1,29 +1,29 @@
 angular.module('Services').factory('Checkin', function ($rootScope, $http) {
 
+    var transform = function(data){
+        return $.param(data);
+    };
+
     return {
 
         create:function(token, pictureURI, options, successCallback, errorCallback) {
-            if(pictureURI != undefined) {
+            if(pictureURI !== undefined) {
                 var ft = new FileTransfer();
                 ft.upload(pictureURI, encodeURI(Settings.host+'checkins/create?token='+token), function(result) {
-                    response = JSON.parse(result.response) // Cause server response is stringified inside result
-                    successCallback(response)
+                    response = JSON.parse(result.response); // Cause server response is stringified inside result
+                    successCallback(response);
                 }, errorCallback, options);
             }
             else {
                 // Ugly fix cause angular sends POST data as JSON in the body instead of params payload
-                var payload = "spot_id="+options.params.spot_id+"&activity="+options.params.activity+"&rating="+options.params.rating
-                if(options.params.comment != undefined) {payload += "&comment="+options.params.comment}
-                if(options.params.riders_ids != undefined) {
-                    payload += "&riders_ids=["+options.params.riders_ids+"]"
+                var payload = "spot_id="+options.params.spot_id+"&activity="+options.params.activity+"&rating="+options.params.rating;
+                if(options.params.comment !== undefined) {payload += "&comment="+options.params.comment;}
+                if(options.params.riders_ids !== undefined) {
+                    payload += "&riders_ids=["+options.params.riders_ids+"]";
                 }
-                $http.post(Settings.host+'checkins/create?token='+token,  payload, {headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}, transformRequest: transform}).success(successCallback).error(errorCallback)
+                $http.post(Settings.host+'checkins/create?token='+token,  payload, {headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}, transformRequest: transform}).success(successCallback).error(errorCallback);
             }
         }
-    }
+    };
 
-    var transform = function(data){
-        return $.param(data);
-    }
-
-})
+});
