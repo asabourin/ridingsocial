4333cd41b44a60390e55d17921d26408d0c0ca7e
Antoine Sabourin <antoine@redant.com.au>
2013-07-30 16:14:45 +1000

    Cleanup after running jshint

--- scripts/services/user.js
+++ scripts/services/user.js
@@ -1,130 +1,138 @@
 angular.module('Services').factory('User', function ($rootScope, $http) {
 
+    "use strict";
+
     // Init
     var id, token, lastCheckinAt, preferences, lastCheckAt;
-    
+
     var persistedUser = JSON.parse(localStorage.getItem('user'));
 
-    if(persistedUser != undefined) {
-        id = persistedUser.id
-        token = persistedUser.token
+    if(persistedUser !== null) {
+        id = persistedUser.id;
+        token = persistedUser.token;
     }
 
     var savedPreferences = JSON.parse(localStorage.getItem('preferences'));
-    if(savedPreferences != undefined) {
-        preferences = savedPreferences
+    if(savedPreferences !== null) {
+        preferences = savedPreferences;
     }
-    else {preferences = new Object({notify: true})}
+    else {
+      preferences = new Object({notify: true});
+    }
 
     var savedLastCheckAt = JSON.parse(localStorage.getItem('lastCheckAt'));
-    if(savedLastCheckAt != undefined) {
-        lastCheckAt = savedLastCheckAt
+    if(savedLastCheckAt !== null) {
+        lastCheckAt = savedLastCheckAt;
     }
-    else {lastCheckAt = new Object({'followed_riders': timestamp(), 'watched_spots': timestamp(), 'notifications': timestamp()})}
+    else {
+      lastCheckAt = new Object({'followed_riders': timestamp(), 'watched_spots': timestamp(), 'notifications': timestamp()});
+    }
 
     //
 
     function savePreferences() {
-        localStorage.setItem('preferences', JSON.stringify(preferences)) 
-        var payload = "notify="+preferences.notify
-        $http.post(Settings.host+'preferences?token='+token, payload, {headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}})
+        localStorage.setItem('preferences', JSON.stringify(preferences));
+        var payload = "notify="+preferences.notify;
+        $http.post(Settings.host+'preferences?token='+token, payload, {headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}});
     }
 
     function saveLastCheckAt() {
-        localStorage.setItem('lastCheckAt', JSON.stringify(lastCheckAt)) 
+        localStorage.setItem('lastCheckAt', JSON.stringify(lastCheckAt));
     }
 
     function persistUser() {
-        var user = {id: id, token:token, lastCheckinAt:lastCheckinAt}
-        localStorage.setItem('user', JSON.stringify(user)) 
+        var user = {id: id, token:token, lastCheckinAt:lastCheckinAt};
+        localStorage.setItem('user', JSON.stringify(user));
     }
 
-    var isLogged = function() {
-        return (token != undefined && token != null)
+    function isLogged() {
+        return (token !== undefined && token !== null);
     }
 
     function timestamp() {
-        return Math.round(new Date().getTime() / 1000)
+        return Math.round(new Date().getTime() / 1000);
     }
 
     //
 
     return {
 
-        isLogged: isLogged,
+        isLogged: function() {
+          isLogged();
+        },
 
         token: function() {
-            return token
+            return token;
         },
 
         get: function() {
-            return {id: id, token:token, lastCheckinAt:lastCheckinAt}
+            return {id: id, token:token, lastCheckinAt:lastCheckinAt};
         },
 
         fetchFollowed: function(successCallback) {
-        $http.get(Settings.host+'riders/followed?token='+token).success(successCallback).error(function(response) {
-            console.log(response)
-            })
+            $http.get(Settings.host+'riders/followed?token='+token).success(successCallback).error(function(response) {
+                console.log(response);
+            });
         },
 
         login:function (accessToken) {
             $http.get(Settings.host+'login?facebook_token='+accessToken).success(function(response) {
-                id = response.id
-                token = response.token
-                persistUser()
-                $rootScope.$broadcast('rs_connected')
+                id = response.id;
+                token = response.token;
+                persistUser();
+                $rootScope.$broadcast('rs_connected');
             }).error(function(response){
-                $rootScope.$broadcast('rs_login_failed', {response:response})
-            })
+                $rootScope.$broadcast('rs_login_failed', {response:response});
+            });
             var phase = $rootScope.$$phase;
-              if(phase == '$apply' || phase == '$digest'){}
-              else {$rootScope.$apply()}  
+            if(phase === '$apply' || phase === '$digest'){}
+            else {$rootScope.$apply();}
         },
-        
+
         registerDevice:function (settings, successCallback, errorCallback) {
-            
+
             if(isLogged) {
-                var payload = "platform="+settings.platform+"&device_token="+settings.token
+                var payload = "platform="+settings.platform+"&device_token="+settings.token;
                 $http.post(Settings.host+'register?token='+token, payload, {headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}}).success(successCallback).error(errorCallback);
             }
             else {
-                errorCallback('No user token')
+                errorCallback('No user token');
             }
         },
 
         setLastCheckinAt: function(time) {
-            lastCheckinAt = time
+            lastCheckinAt = time;
         },
 
         updateLastCheckAt: function(what) {
-            lastCheckAt[what] = timestamp()
-            saveLastCheckAt()
+            lastCheckAt[what] = timestamp();
+            saveLastCheckAt();
         },
 
         lastCheckAt: function(what) {
-            return lastCheckAt[what]
+            return lastCheckAt[what];
         },
 
         checkNewNotifications: function(successCallback) {
-            var params ='last_check_notifications='+lastCheckAt['notifications']+'&last_check_riders='+lastCheckAt['followed_riders']+'&last_check_spots='+lastCheckAt['watched_spots']
+            var params ='last_check_notifications='+lastCheckAt.notifications+'&last_check_riders='+lastCheckAt.followed_riders+'&last_check_spots='+lastCheckAt.watched_spots;
             $http.get(Settings.host+'notifications/new_since?token='+token+'&'+params).success(successCallback).error(function(response) {
-                console.log(response)
-            })
+                console.log(response);
+            });
         },
 
         updatePreferences: function(prefs) {
-            preferences = prefs
-            savePreferences()
+            preferences = prefs;
+            savePreferences();
         },
 
         getPreferences: function() {
-            return preferences
+            return preferences;
         },
 
         notifications: function(successCallback) {
             $http.get(Settings.host+'notifications?token='+token).success(successCallback).error(function(response) {
-                console.log(response)
-            })
+                console.log(response);
+            });
         },
 
         logout: function() {
@@ -132,9 +140,9 @@ angular.module('Services').factory('User', function ($rootScope, $http) {
             id = null;
             $rootScope.user = null;
             $rootScope.position = null;
-            localStorage.clear()
+            localStorage.clear();
         }
 
-    }
+    };
 
-})
+});
