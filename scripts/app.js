c370fe73e1a90b9d587285208b70fd97ee45dea1
Antoine Sabourin <antoine@redant.com.au>
2013-07-30 17:04:12 +1000

    Cleanup and refactor after running jshint

--- scripts/app.js
+++ scripts/app.js
@@ -49,32 +49,6 @@ angular.element(document).ready(function() {
   injector = angular.bootstrap(document, ['App']);
 });
 
-//Fastclick
-angular.module('App').directive("ngTap", function() {
-  return function($scope, $element, $attributes) {
-    var tapped = false;
-    $element.bind("touchstart", function(event) {
-      return tapped = true;
-    });
-    $element.bind("touchmove", function(event) {
-      tapped = false;
-      return event.stopImmediatePropagation();
-    });
-    return $element.bind("touchend", function() {
-      if (tapped) {
-        return $scope.$apply($attributes["ngTap"]);
-      }
-    })
-  }
-})
-
-// Timeago filter using Moment.js
-angular.module('App').filter('fromNow', function() {
-  return function(date) {
-    return moment(date).fromNow();
-  }
-});
-
 // Forward app resumed event to angular
 document.addEventListener("resume", function() {
   var scope = angular.element(document).scope();
--- scripts/directives.js
+++ scripts/directives.js
@@ -1,11 +1,25 @@
-angular.module('App')
+//Fastclick
+angular.module('App').directive("ngTap", function() {
+  return function($scope, $element, $attributes) {
+    var tapped = false;
+    $element.bind("touchstart", function(event) {
+      return tapped = true;
+    });
+    $element.bind("touchmove", function(event) {
+      tapped = false;
+      return event.stopImmediatePropagation();
+    });
+    return $element.bind("touchend", function() {
+      if (tapped) {
+        return $scope.$apply($attributes.ngTap);
+      }
+    });
+  };
+});
 
-.directive('ngTouch', function () {
-        return function ($scope, $element, $attrs) {
-            $element.bind('touchend', function () {
-                if(!$scope.$$phase) {
-                    $scope.$apply($attrs['ngTouch']);
-                }
-            });
-        };
-})
+// Timeago filter using Moment.js
+angular.module('App').filter('fromNow', function() {
+  return function(date) {
+    return moment(date).fromNow();
+  };
+});
