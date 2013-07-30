6253c3f5d7bc1b8a023e032ac6319391d15c5bb3
Antoine Sabourin <antoine@redant.com.au>
2013-07-31 09:24:12 +1000

    Cleanup after running jshint

--- scripts/controllers/sessions.js
+++ scripts/controllers/sessions.js
@@ -1,13 +1,11 @@
 angular.module('App')
   .controller('SessionController', function(Sessions, User, Spots, $scope, $rootScope) {
 
-
-    //
-
     $scope.showOthers = function (session_id) {
-      $rootScope.this_session = $scope.session
+      $rootScope.this_session = $scope.session;
       $rootScope.othersOpen = true;
     };
+
     $rootScope.hideOthers = function () {
       $rootScope.othersOpen = false;
     };
@@ -15,45 +13,44 @@ angular.module('App')
     $scope.toggleLikeSession = function() {
       if($scope.session.liked) {
         Sessions.unlike(User.token(), $scope.session.id, function(response) {
-          $scope.session.liked = false
-          $scope.session.nb_likes -= 1
-        })
+          $scope.session.liked = false;
+          $scope.session.nb_likes -= 1;
+        });
       }
       else {
         Sessions.like(User.token(), $scope.session.id, function(response) {
-          $scope.session.liked = true
-          $scope.session.nb_likes += 1
-        })
+          $scope.session.liked = true;
+          $scope.session.nb_likes += 1;
+        });
       }
-    }
+    };
 
     $scope.showComments = function () {
-	$rootScope.session = $scope.session
+      $rootScope.session = $scope.session;
       Sessions.comments($scope.session.id, function(response) {
-        $rootScope.comments = response
-      })
+        $rootScope.comments = response;
+      });
       Sessions.likes($scope.session.id, function(response) {
-        $rootScope.likes = response
-      })
+        $rootScope.likes = response;
+      });
       $rootScope.commentsOpen = true;
     };
+
     $scope.hideComments = function () {
-      $rootScope.comments = []
-      $rootScope.session = null
+      $rootScope.comments = [];
+      $rootScope.session = null;
       $rootScope.commentsOpen = false;
     };
 
     $scope.postComment = function(session_id) {
-    	if($scope.reply && !$scope.posting_comment) {
-    		$scope.post_comment_loading = true
-    		Sessions.postComment(User.token(), session_id, $scope.reply, function(response){
-    			$rootScope.comments.push(response)
-    			$scope.posting_comment = false
-    			$scope.reply = ''
-    		})
-    	}
-    }
+      if($scope.reply && !$scope.posting_comment) {
+        $scope.post_comment_loading = true;
+        Sessions.postComment(User.token(), session_id, $scope.reply, function(response){
+          $rootScope.comments.push(response);
+          $scope.posting_comment = false;
+          $scope.reply = '';
+        });
+      }
+    };
 
-    
-
-})
+});
